f0512376521cddbc2386157ac3ce74c285c3cbaa
VAdIM1199 <v-dojnikov@mail.ru>
2025-09-26 16:45:41 +0300

    Add files via upload

---
+++ tests/test_calculator_program.py
@@ -0,0 +1,29 @@
+import pytest
+from calculate.calculator_program import calculate
+
+def test_calculate_addition():
+    assert calculate(1, 1, '+') == 2
+
+def test_calculate_division():
+    assert calculate(8, 2, '/') == 4
+
+def test_calculate_minus():
+    assert calculate(5, 3, '-') == 2
+
+def test_calculate_ymnog():
+    assert calculate(4, 3, '*') == 12
+
+def test_calculate_unknown_operation():
+    assert calculate(5, 5, 'unknown') == "Неизвестная операция."
+
+def test_calculate_division_by_zero():
+    assert calculate(5, 0, '/') == "На ноль делить нельзя."
+
+'''
+Задача. В настоящий момент реализовано три unit-теста
+Проверяется корректность работы калькулятора для действий сложения, деления и неизвестной операции
+Необходимо, как минимум, добавить тесты для следующих операций:
+1. Вычитание
+2. Умножение
+Но будет круто, если ты сможешь придумать и добавить дополнительные тесты
+'''
